c9fa32f79b7f7f5644b3e8504eb906267e8df7df
Christian Carhuachin <cristiancarhuachin@gmail.com>
2026-09-05 19:20:48 -0500

    refactor: streamline searchedTasks logic in useTask hook for improved readability

--- src/hooks/useTask.ts
+++ src/hooks/useTask.ts
@@ -1,29 +1,19 @@
 import { useState } from 'react';
 import type { TaskItemProps } from '../types';
 import { useLocalStorage } from './useLocalStorage';
-/*
-const tasks: TaskItemProps[] = [
-  { title: 'Esta es mi primera tarea', completed: false },
-  { title: 'Comprar leche', completed: true },
-  { title: 'responder correos pendientes importantes puntuales', completed: false },
-];
-*/
+
 function useTask() {
   const { item: myTasks, saveItem: setMyTasks } = useLocalStorage('TASKS_V1', []);
   const [searchTask, setSearchTask] = useState('');
   const [openModal, setOpenModal] = useState(false);
 
-  let searchedTasks: TaskItemProps[] = [];
-
-  if (!(searchTask.length >= 1)) {
-    searchedTasks = myTasks;
-  } else {
-    searchedTasks = myTasks.filter((task) => {
-      const taskText = task.title.toLowerCase();
-      const searchText = searchTask.toLowerCase();
-      return taskText.includes(searchText);
-    });
-  }
+  const searchedTasks: TaskItemProps[] = !(searchTask.length >= 1)
+    ? myTasks
+    : myTasks.filter((task) => {
+        const taskText = task.title.toLowerCase();
+        const searchText = searchTask.toLowerCase();
+        return taskText.includes(searchText);
+      });
 
   function completeTask(title: string) {
     const updatedTasks = myTasks.map((task) => {
