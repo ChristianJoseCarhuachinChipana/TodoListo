4282f3772eccb985038c6ffd158c0a86fb8437aa
Christian Carhuachin <cristiancarhuachin@gmail.com>
2026-09-06 01:29:16 -0500

    style: update layout styles for improved responsiveness in App component

--- src/App.tsx
+++ src/App.tsx
@@ -22,8 +22,8 @@ function App() {
 
   return (
     <>
-      <div className="flex items-center justify-center py-2 bg-todo-listo min-h-screen font-average-sans">
-        <div className="flex flex-wrap items-center justify-center gap-3 max-w-md md:flex-row">
+      <div className="flex items-center justify-center py-2 bg-todo-listo min-h-dvh font-average-sans">
+        <div className="flex flex-wrap items-center justify-center gap-3 max-w-md px-5 md:flex-row sm:px-0">
           <img src={logo} alt="Todo Listo" className="w-40 h-auto rounded-2xl overflow-hidden" />
           <SearchBar searchTask={searchTask} setSearchTask={setSearchTask} />
           <TaskSection
